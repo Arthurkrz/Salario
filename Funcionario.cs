b3675ed7a296de483af754fbc5c1628383a537cf
Arthur K <arthurk031@outlook.com>
2024-03-31 20:31:28 -0300

    Correção

---
+++ Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Salario
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+        public double Imposto;
+        public double CalcularSalarioLiquido()
+        {
+            return Salario - (Salario * Imposto / 100);
+        }
+        public void AumentarSalario(double incremento)
+        {
+            Salario += Salario * incremento / 100;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,35 +6,23 @@ namespace Salario
     {
         static void Main(string[] args)
         {
-            string nome;
-            double salariobruto;
-            double salarioliquido;
-            double imposto;
-            double incremento;
-            bool controle = true;
-            while (controle)
+            Funcionario funcionario = new Funcionario();
+            Console.WriteLine("Insira o nome do funcionário: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.WriteLine("Insira o salário do funcionário: ");
+            funcionario.Salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o valor do imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+            if (funcionario.Imposto > 0 && funcionario.Salario > 0)
             {
-                Console.WriteLine("Insira o nome do funcionário: ");
-                nome = Console.ReadLine();
-                Console.WriteLine("Insira o salário do funcionário: ");
-                salariobruto = double.Parse(Console.ReadLine());
-                Console.WriteLine("Insira o valor do imposto: ");
-                imposto = double.Parse(Console.ReadLine());
-                if (imposto > 0 && salariobruto > 0)
-                {
-                    double imposto2 = imposto / 100;
-                    Console.WriteLine($"Funcionário {nome}.");
-                    salarioliquido = salariobruto - (salariobruto * imposto2);
-                    Console.WriteLine($"{nome} ganha R${salarioliquido} por mês.");
-                    Console.WriteLine("Qual o valor em % a ser adicionado ao salário?");
-                    incremento = double.Parse(Console.ReadLine());
-                    double valorincremento = salariobruto * (incremento / 100);
-                    salariobruto += valorincremento;
-                    double imposto3 = imposto / 100;
-                    salarioliquido = salariobruto - (salariobruto * imposto3);
-                    Console.WriteLine($"{nome} agora ganha R${salarioliquido} por mês.");
-                    controle = false;
-                }
+                Console.WriteLine($"Funcionário {funcionario.Nome}.");
+                double salarioLiquido = funcionario.CalcularSalarioLiquido();
+                Console.WriteLine($"{funcionario.Nome} ganha R${salarioLiquido} por mês.");
+                Console.WriteLine("Qual o valor em % a ser adicionado ao salário?");
+                double valorIncremento = double.Parse(Console.ReadLine());
+                funcionario.AumentarSalario(valorIncremento);
+                salarioLiquido = funcionario.CalcularSalarioLiquido();
+                Console.WriteLine($"{funcionario.Nome} agora ganha R${salarioLiquido} por mês.");
             }
         }
     }
